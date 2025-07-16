0a39c3c33d1d6a62443f3437d4fe9e0dede8fdf5
luyb177 <2085661244@qq.com>
2025-07-16 16:04:21 +0800

    Site updated: 2025-07-16 16:04:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/08/2025-07-08-go-testing-mock/"];function toRandomPost(){
+var posts=["2025/07/08/2025-07-08-go-testing-mock/","2025/07/16/2025-07-16-redis/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
