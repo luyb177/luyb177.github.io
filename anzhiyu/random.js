4606e2c2cd31767a1651ea264d6052633e87df11
luyb177 <2085661244@qq.com>
2025-10-28 17:00:57 +0800

    Site updated: 2025-10-28 17:00:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/08/2025-07-08-go-testing-mock/","2025/07/16/2025-07-16-redis/"];function toRandomPost(){
+var posts=["2025/07/08/2025-07-08-go-testing-mock/","2025/07/16/2025-07-16-redis/","2025/10/28/2025-10-28-k8s-pod/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
